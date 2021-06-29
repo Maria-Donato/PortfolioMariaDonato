b30d650a2384065fe6982bfea0cbb771d79dfa47
Maria Donato <86387354+Maria-Donato@users.noreply.github.com>
2021-06-29 20:55:45 -0300

    Delete js directory

--- js/java.js
+++
@@ -1,7 +0,0 @@
-function cambiarClase(){
-   let siteNav = document.getElementById ("site-nav")
-   siteNav.classList.toggle ("site-nav-open")
-
-   let menuOpen =document.getElementById("menu-togle")
-   menuOpen.classList.toggle("menu-open")
-}
